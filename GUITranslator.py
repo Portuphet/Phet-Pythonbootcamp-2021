55b71e9991fb12216eef2603aec4cb1a7edd77fc
Phet THEPVONGSA <74132962+Portuphet@users.noreply.github.com>
2020-12-13 17:31:15 +0700

    Update GUITranslator.py

--- GUITranslator.py
+++ GUITranslator.py
@@ -1,43 +1,45 @@
-# GUITranslator.py
-from tkinter import *
-#จากไลบรารีชื่อ tkinter, * คือให้ดึงความสามารถหลักมาทั้งหมด
+#GUItranslator.py
+from tkinter import * #จากไลบรารีชื่อ tkinter, * ตือให้ดึงความสามารถหลักมาทังหมด
 from tkinter import ttk # ttk is theme of tk
-### -------Google Translate---------
+###--------------------------Google Translate----------------------
 from googletrans import Translator
-translator = Translator() #สร้างฟังชั่นแปลภาษา
+translator = Translator ( )      
 
-GUI = Tk() #สร้างหน้าต่างหลัก
-GUI.geometry('500x300') #กว้าง x สูง
-GUI.title('โปรแกรมแปลภาษา by Uncle Engineer')
-# -----config-----
-FONT = ('Angsana New',15)
-# -----Label------
-L = ttk.Label(GUI,text='กรุณากรอกคำศัพท์ที่ต้องการแปล',font=FONT)
-L.pack()
-# -----Entry (ช่องกรอกข้อความ)-----
-v_vocab = StringVar() #กล่องเก็บข้อความ
+GUI = Tk ( ) #สร้างหน้าต่างหลัก
+GUI.geometry ( '500x300' ) #กว้าง x สูง
+GUI.title ('โปรแกรมแปลภาษา by Phet THEPVONGSA' )
+# --------------- Config------------------
+FONT = ( ' Angsana New ' , 15 )
 
-E1 = ttk.Entry(GUI,textvariable = v_vocab,font=FONT,width=40)
-E1.pack(pady=20)
+# --------------- Label-------------------
+L = ttk.Label (GUI, text = 'กรุณากรอกคำศัพท์ที่ต้องการแปล', font=FONT)
+L.pack ( pady=10 )
 
-# -----Button (ปุ่มแปล)-----
-def Translate():
-    vocab = v_vocab.get() #.get คือให้แสดงผลออกมา
-    meaning = translator.translate(vocab,dest='th')
-    print( vocab + ' : ' + meaning.text)
-    print( meaning.pronunciation)
-    v_result.set(vocab + ' : ' + meaning.text) #สั่งโชว์ใน gui
+# ---------------Entry (ช่องกรอกข้อความ)-----------------
+v_vocab = StringVar ( ) # กล่องเก็บข้อความ
 
-B1 = ttk.Button(GUI,text='Translate',command=Translate) #สร้างปุ่มขึ้นมา
-B1.pack(ipadx=20,ipady=10) # show ปุ่มขึ้นมาวางจากบนลงล่าง
-# -----Label------
-L = ttk.Label(GUI,text='คำแปล',font=FONT)
-L.pack()
-# -----Result-----
-v_result = StringVar() #นี่คือกล่องสำหรับเก็บคำแปล
-FONT2 = ('Angsana New',20)
-R1 = ttk.Label(GUI,textvariable=v_result, font=FONT2, foreground='green')
-R1.pack()
+E1 = ttk.Entry(GUI, textvariable = v_vocab, font=FONT) #สร้างช่องกรอกข้อความ และ กำหนดให้รับข้อความจากกล่องข้อความ
+E1.pack (ipadx=40, ipady=5, pady=20) # show ช่่องกรอก
+
+# ----------------Button--------------------
+def Translate ( ):
+      vocab = v_vocab.get ( ) # .get คือคำสั่งให้แสดงผลออกมาเนื่องจากไม่สามารถเรียนได้โดยตรง
+      meaning = translator.translate ( vocab, dest = 'th' )
+      print ( vocab + ' : ' + meaning.text )
+      v_result.set ( meaning.text )
+
+B1 = ttk.Button ( GUI,text = 'Translate', command=Translate ) #สร้างปุ่มขึ้นมา
+B1.pack (ipadx=20, ipady=10 ) # show ปุ่มขึ้นมาวางจากบนลงล่าง
+
+# --------------- Label-------------------
+L = ttk.Label (GUI, text = 'คำแปล', font=FONT)
+L.pack ( pady=20)
+
+# --------------- Result------------------
+v_result = StringVar ( )
+FONT2 = ( ' Cordia New ' , 15)
+E2 = ttk.Entry ( GUI, textvariable = v_result , font = FONT2 , foreground = 'green')
+E2.pack ( ipadx = 40, ipady = 5)
 
 
-GUI.mainloop() #ทำให้โปรแกรมรันได้ตลอดเวลาจนกว่าจะปิด (บรรทัดสุดท้าย)
+GUI.mainloop ( ) # ทำให้โปรแกรมรันได้ตลอดเวลาจนกว่าจะปิด
